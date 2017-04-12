ba6d2e4af420a6e774393a756e45da74a908e21f
Sunghyon Kyeong <sunghyon.kyeong@gmail.com>
2017-04-12 10:23:36 +0900

    Group Mean 코드 약간 수정.

--- gait/anal_group_mean.m
+++ gait/anal_group_mean.m
@@ -30,31 +30,55 @@ T_nerve_root = prepare_skimed_data(analVars, 8, 2);  % nerve_root_flip
 
 
 
-% Correlation Analysis
+% Group Mean
 %--------------------------------------------------------------------------
-varName = analVars{37};
-muHemi = mean(T_hemi.(varName));
-seHemi = std(T_hemi.(varName))/sqrt(size(T_hemi,1));
-
-muPain = mean(T_pain.(varName));
-sePain = std(T_pain.(varName))/sqrt(size(T_pain,1));
-
-muNormal = mean(T_normal.(varName));
-seNormal = std(T_normal.(varName))/sqrt(size(T_normal,1));
-
-muParkinson = mean(T_parkinson.(varName));
-seParkinson = std(T_parkinson.(varName))/sqrt(size(T_parkinson,1));
-
-muNerveRoot = mean(T_nerve_root.(varName));
-seNerveRoot = std(T_nerve_root.(varName))/sqrt(size(T_nerve_root,1));
-
-fout = ['~/Desktop/' varName '.csv'];
+fout = '~/Desktop/group_mean.csv';
 fid = fopen(fout,'w+');
-fprintf(fid,'%s,Mean,SD\n',varName);
-fprintf(fid,'Hemi,%f,%f\n',muHemi,seHemi);
-fprintf(fid,'Pain,%f,%f\n',muPain,sePain);
-fprintf(fid,'NerveRoot,%f,%f\n',muNerveRoot,seNerveRoot);
-fprintf(fid,'Parkinson,%f,%f\n',muParkinson,seParkinson);
-fprintf(fid,'Normal,%f,%f\n',muNormal,seNormal);
+fprintf(fid,'Variable,Hemi,Pain,NerveRoot,Parkinson,Normal\n');
+for i=1:length(analVars)
+    varName = analVars{i};
+    muHemi = mean(T_hemi.(varName));
+    sdHemi = std(T_hemi.(varName));
+    
+    muPain = mean(T_pain.(varName));
+    sdPain = std(T_pain.(varName));
+    
+    muNormal = mean(T_normal.(varName));
+    sdNormal = std(T_normal.(varName));
+    
+    muParkinson = mean(T_parkinson.(varName));
+    sdParkinson = std(T_parkinson.(varName));
+    
+    muNerveRoot = mean(T_nerve_root.(varName));
+    sdNerveRoot = std(T_nerve_root.(varName));
+    fprintf(fid,'%s, %.2f aa %.2f,%.2f aa %.2f, %.2f aa %.2f, %.2f aa %.2f, %.2f aa %.2f\n',varName,muHemi,sdHemi,muPain,sdPain,muNerveRoot,sdNerveRoot,muParkinson,sdParkinson,muNormal,sdNormal);
+    
+end
 fclose(fid);
+
+
+% varName = analVars{37};
+% muHemi = mean(T_hemi.(varName));
+% seHemi = std(T_hemi.(varName))/sqrt(size(T_hemi,1));
+%
+% muPain = mean(T_pain.(varName));
+% sePain = std(T_pain.(varName))/sqrt(size(T_pain,1));
+%
+% muNormal = mean(T_normal.(varName));
+% seNormal = std(T_normal.(varName))/sqrt(size(T_normal,1));
+%
+% muParkinson = mean(T_parkinson.(varName));
+% seParkinson = std(T_parkinson.(varName))/sqrt(size(T_parkinson,1));
+%
+% muNerveRoot = mean(T_nerve_root.(varName));
+% seNerveRoot = std(T_nerve_root.(varName))/sqrt(size(T_nerve_root,1));
+% fout = ['~/Desktop/' varName '.csv'];
+% fid = fopen(fout,'w+');
+% fprintf(fid,'%s,Mean,SD\n',varName);
+% fprintf(fid,'Hemi,%f,%f\n',muHemi,seHemi);
+% fprintf(fid,'Pain,%f,%f\n',muPain,sePain);
+% fprintf(fid,'NerveRoot,%f,%f\n',muNerveRoot,seNerveRoot);
+% fprintf(fid,'Parkinson,%f,%f\n',muParkinson,seParkinson);
+% fprintf(fid,'Normal,%f,%f\n',muNormal,seNormal);
+% fclose(fid);
 
